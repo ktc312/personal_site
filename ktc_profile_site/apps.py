ceed161a70b7c7c1532011ecd2939a7818f61d87
ktc312 <ktc312@nyu.edu>
2017-12-02 19:33:41 -0500

    first draft

---
+++ ktc_profile_site/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class KtcProfileSiteConfig(AppConfig):
+    name = 'ktc_profile_site'
---
+++ ktc_profile_site/views.py
@@ -0,0 +1,16 @@
+#  -*- coding: utf-8 -*-
+# coding: utf-8
+from django.shortcuts import render
+import sys
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def test(request):
+    return render(request, 'test.html')
+
+
+def test2(request):
+    return render(request, 'test2.html')
